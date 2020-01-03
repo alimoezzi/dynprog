cea24bf0e1f5eacec50f62bfb285253c9f4db98c
ALi <s.arme@ya.ru>
2020-01-04 00:05:59 +0330

    fix keeping infinity on adding it to an int

--- Floyd/floyd.cpp
+++ Floyd/floyd.cpp
@@ -1,11 +1,34 @@
 #include<cstdio>
+#include<vector>
+#include<algorithm>
+#pragma warning(disable:4996)
+
+
+using namespace std;
+vector<vector<int>> floyd(vector<vector<int>>);
 
 int main() {
-	int a[4][4] = {
-		{5,4,8,8},
-		{7,1,7,5},
-		{2,12,35,4},
-		{51,2,8,7}
+	vector<vector<int>> a = {
+		{0,-1,3,-1},
+		{2,0,-1,-1},
+		{-1,7,0,1},
+		{6,-1,-1,0}
 	};
+	auto test = floyd(a);
+	for (auto i :test) {
+		for (auto j : i) printf("%d ",j);
+		puts("");
+	}
 	return 0;
+}
+
+vector<vector<int>> floyd(vector<vector<int>> w) {
+	for (int k = 0; k < w.size(); k++) {
+		for (int i = 0; i < w.size(); i++) {
+			for (int j = 0; j < w.size(); j++) {
+				w[i][j] = min((uint32_t)w[i][j],w[i][k] >0 && w[k][j] > 0 ? (uint32_t)w[i][k] + (uint32_t)w[k][j] : (uint32_t) -1);
+			}
+		}
+	}
+	return w;
 }
